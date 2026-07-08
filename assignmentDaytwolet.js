1b44f6dfff3ea58962cfa00044ccf06b9ece3aca
ashikapersonalmail-collab <ashikapersonalmail@gmail.com>
2026-07-08 16:59:40 +0530

    Add files via upload
    day2primitiveDatatypes

---
+++ assignmentDaytwolet.js
@@ -0,0 +1,18 @@
+let userName ="Ashika"     
+let organization = "Tech"
+let contactNumber = 7865432896
+let knowsAutomation = false
+let usePlaywright 
+
+console.log("UserName:"+" "+userName)
+console.log("TypeOf UserName:"+" "+typeof userName)
+console.log("Organization:"+ " "+organization)
+console.log("TypeOf Organization:"+" "+typeof organization)
+console.log("ContactNumber:"+ " "+ contactNumber)
+console.log("KnowsAutomation:"+ " "+ knowsAutomation)
+console.log("TypeOf KnowsAutomation:"+" "+typeof knowsAutomation)
+console.log("UsePlaywright:"+ " "+ usePlaywright)
+console.log("TypeOf UsePlaywright:"+" "+typeof usePlaywright)
+
+
+
