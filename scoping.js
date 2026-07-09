20e8c2d68e9fc182ca5a46a0c165bbce6938708b
ashikapersonalmail-collab <ashikapersonalmail@gmail.com>
2026-07-09 08:46:11 +0530

    Add files via upload
    scopingWork_aroundd

---
+++ scoping.js
@@ -0,0 +1,20 @@
+//const { log } = require("console");
+
+
+let genderType = "male"
+function printGender()
+
+{
+    let colour = "brown"
+    
+    if(genderType=="female")
+    {
+
+        var age=30
+        let  colour = "pink"
+        console.log(colour);
+    }
+console.log(age);
+}
+printGender()
+console.log(genderType);
