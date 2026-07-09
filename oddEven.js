85a30fe3bcf2dcc8f6ef342dff2e8bb5bf16bab9
ashikapersonalmail-collab <ashikapersonalmail@gmail.com>
2026-07-09 18:33:56 +0530

    day3Assignment

---
+++ oddEven.js
@@ -0,0 +1,44 @@
+function isOddOrEven(number)
+{
+    number = 100
+    if (number/2) {
+        console.log(number+":"+"Number is Eeven number");
+    
+    }
+    else
+    {console.log("number is odd number")
+
+    }
+    
+}
+
+
+function isOddOrEvenModulus(number1)
+{
+number1 = 13
+if (number1%2==0) {
+    console.log(number1+":"+ "Even Number");
+    
+}
+else
+{
+    console.log(number1+":"+"Odd Nnumber ");
+    
+}
+}
+isOddOrEven()
+isOddOrEvenModulus()
+
+function isOddOrEvendDec(number3)
+{
+    if (number3%2==0) {
+        console.log(number3+":"+"Number is Even number");
+    }
+    else{
+        console.log(number3+":"+"Number is Odd Number");
+        
+    }
+}
+isOddOrEvendDec(11)
+isOddOrEvendDec(12)
+isOddOrEvendDec(88)
