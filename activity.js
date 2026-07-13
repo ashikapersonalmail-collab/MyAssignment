880d774a19c627ac6481eb2834902691481f87c5
ashikapersonalmail-collab <ashikapersonalmail@gmail.com>
2026-07-13 10:50:20 +0530

    dailyActivity

---
+++ activity.js
@@ -0,0 +1,9 @@
+// loop to count number from 1 to 25
+for (let i = 1; i <= 25; i++) {
+    // i modulus not eqyals to zero to find odd or even 
+     while (i%2!=0) {
+        // to print odd numbers
+        console.log(i+":"+"odd number");
+        i++
+     }
+}
---
+++ squareInReverserde.js
@@ -0,0 +1,9 @@
+//loop to reverse the number from 10 to 1
+for (let index = 10; index > 0; index--) {
+    // to square the value
+    var i=index*index
+    // to print the squared value of i
+    console.log("Square of:"+index+"="+i);
+    
+    
+}
